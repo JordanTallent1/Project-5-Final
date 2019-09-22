f806361916866826b8d26579d47089595d87816e
JordanTallent1 <40626384+JordanTallent1@users.noreply.github.com>
2019-09-21 20:22:12 -0400

    Update upload_docker.sh

--- upload_docker.sh
+++ upload_docker.sh
@@ -7,9 +7,11 @@
 # Create dockerpath
 dockerpath=project5
 
-# Step 2:  
+# Step 2:
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
+cat ./my_password.txt| docker login --username jtallent2019 --password-stdin
+docker tag project5 jtallent2019/project5:version1.0
 
 # Step 3:
 docker push jtallent2019/project5
